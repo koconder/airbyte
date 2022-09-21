6ce3c2bf03a7a329c4f9baf03bbb3c2a74df8d1a
jdpgrailsdev <jpearlin1@gmail.com>
2022-09-21 15:18:29 -0400

    Organize tracing of workflows and activities

--- airbyte-workers/src/main/java/io/airbyte/workers/temporal/sync/SyncWorkflowImpl.java
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/sync/SyncWorkflowImpl.java
@@ -41,7 +41,7 @@ public class SyncWorkflowImpl implements SyncWorkflow {
   @TemporalActivityStub(activityOptionsBeanName = "shortActivityOptions")
   private PersistStateActivity persistActivity;
 
-  @Trace(operationName = "workflow.sync")
+  @Trace(operationName = "workflow", resourceName = "SyncWorkflowImpl")
   @Override
   public StandardSyncOutput run(final JobRunConfig jobRunConfig,
                                 final IntegrationLauncherConfig sourceLauncherConfig,
